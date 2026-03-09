9fe635ebe337bcde412cee297b41cc79c98498e8
danjwilko <danielwilkinson0991@gmail.com>
2026-03-09 13:03:19 +0000

    Formatting updates

--- sn_project/urls.py
+++ sn_project/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
