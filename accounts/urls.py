a5251bfd6b4fe2c46305f71913a895fc746edf8c
danjwilko <danielwilkinson0991@gmail.com>
2026-09-13 21:23:08 +0100

    Updated requirments and url mismatch

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,6 @@
 
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from django_ratelimit.decorators import ratelimit
 
 from . import views
 
@@ -12,7 +11,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     # Default auth urls for login and logout.
     # Login
-    path("login/", views.RateLimitedLoginView.as_view(), name="login"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
     # Logout
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # Custom password reset - Uses Django's built-in views but with
